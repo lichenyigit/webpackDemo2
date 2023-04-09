3925abfc30387b6386564ac9cbaddd05bd508cda
ChanYeeLi <lichenyiwin@outlook.com>
2023-04-10 00:32:19 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+console.log(path.resolve(__dirname, 'dist'))
+module.exports={
+    //入口文件
+    entry: './src/index.js',
+    output: {
+        //输出文件名称
+        filename: 'main.js',
+        //输出路径（绝对路径）
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',//development production
+    module: {
+        //对某个格式的文件进行转换
+        rules: [
+            {
+                test: /\.css$/,//正则表达式
+                use: [//use中的loader顺序是从下到, 依次执行
+                    //js央视内容写入style标签
+                    "style-loader",
+                    //css文件转为js
+                    'css-loader',
+                ]
+            }
+        ],
+    },
+}
